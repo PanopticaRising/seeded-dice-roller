c8fe23e873fc0316d1bff9de3c6d960b5f638d2f
Panopticon <gibryon@protonmail.com>
2021-08-18 15:44:06 -0400

    Fixed dependency on hacked random packages

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ use crossterm::{
 };
 use rand_pcg::Pcg64;
 use rand_seeder::Seeder;
-use std::{error::Error, io::{stdout}, sync::mpsc};
+use std::{error::Error, fs::{File, OpenOptions}, io::{BufRead, BufReader, stdout}, sync::mpsc};
 use tui::{Terminal, backend::CrosstermBackend};
 
 use clap::{AppSettings, Clap};
 
 use crate::utils::{app::App, user_interface::Event};
+use std::io::Write;
+use lazy_static::lazy_static;
+use regex::Regex;
+
 
 
 /// This is a simple CLI dice roller. It optionally allows you to specify a seed for a fun repeatable experience.
@@ -23,12 +27,46 @@ struct Opts {
     /// A string (word, sentence, etc) to initialize the random generator.
     #[clap(short, long)]
     seed: String,
+    // TODO: Add an option that lets people opt in/out of dumping the state.
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let opt = Opts::parse();
+    let save_file = format!("{}.seed.state", opt.seed);
+    
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"\((\d+), (\d+)\)").unwrap();
+    }
 
-    let mut rng: Pcg64 = Seeder::from(opt.seed).make_rng();
+    let file = File::open(&save_file);
+    
+    let mut rng: Pcg64 = if let Ok(f) = file {
+        let reader = BufReader::new(f);
+        let lines = reader.lines();
+        let mut peek = lines.peekable();
+        let mut tup: Option<(u128, u128)> = None;
+
+        while let Some(line) = peek.next() {
+            if peek.peek().is_none() {
+                let line = line.unwrap();
+                let capa = RE.captures_iter(&line).next();
+                if let Some(cap) = capa {
+                    let state: u128 = u128::from_str_radix(&cap[1], 10).unwrap();
+                    let incr: u128 =  u128::from_str_radix(&cap[2], 10).unwrap();
+                    tup = Some((state, incr));
+                }
+            }
+        };
+
+        if let Some(tup) = tup {
+            Pcg64::reinit(tup.0, tup.1)
+        } else {
+            Seeder::from(opt.seed).make_rng()
+        }
+    } else {
+        Seeder::from(opt.seed).make_rng()
+    };
+
     enable_raw_mode()?;
 
     let mut stdout = stdout();
@@ -65,6 +103,10 @@ fn main() -> Result<(), Box<dyn Error>> {
                 KeyCode::Down => app.items.next(),
                 KeyCode::Up => app.items.previous(),
                 KeyCode::Right => app.items.next(),
+                KeyCode::Char('s') => {
+                    let mut file = OpenOptions::new().append(true).write(true).create(true).open(&save_file)?;
+                    write!(file, "{:?}\n", rng.dump())?;
+                }
                 _ => {}
             },
             Event::Tick => {}
